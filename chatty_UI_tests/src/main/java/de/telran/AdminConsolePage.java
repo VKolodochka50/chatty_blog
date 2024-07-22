fc1fd8307d9b3e9f2d438bc77bed50f5dc441a23
Valerych <valeriy.kolodochka@gmail.com>
2024-07-22 18:17:01 +0200

    Created initial framework structure. Attention - ChromeRegistry class implemented with method capable to auto-download and initialize necessary driver (Chrome driver). Login Page contains all necessary elements and interact methods. Other pages to follow

---
+++ chatty_UI_tests/src/main/java/de/telran/AdminConsolePage.java
@@ -0,0 +1,10 @@
+package de.telran;
+
+import org.openqa.selenium.WebDriver;
+
+public class AdminConsolePage  extends BasePage {
+
+    public AdminConsolePage(WebDriver driver) {
+        super(driver);
+    }
+}
